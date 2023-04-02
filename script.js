718eb7b6a8a5b6fa50c3348e6d16e013a0fc13ac
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-04-03 02:06:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+// Select elements
+const gridContainer = document.querySelector('#grid-container');
+const blockIdInput = document.querySelector('#block_id');
+const colorInput = document.querySelector('#colour_id');
+const changeButton = document.querySelector('#change_button');
+const resetButton = document.querySelector('#reset_button');
+
+// Add event listeners
+changeButton.addEventListener('click', changeColor);
+resetButton.addEventListener('click', resetColors);
+
+// Functions
+function changeColor() {
+  const blockId = blockIdInput.value;
+  const color = colorInput.value;
+
+  // Reset all grid items to transparent
+  const gridItems = gridContainer.querySelectorAll('.grid-item');
+  gridItems.forEach(item => item.style.backgroundColor = 'transparent');
+
+  // Change background color of specified block id
+  const block = document.querySelector(`#${blockId}`);
+  if (block) {
+    block.style.backgroundColor = color;
+  } else {
+    alert('Invalid block ID');
+  }
+}
+
+function resetColors() {
+  // Reset all grid items to transparent
+  const gridItems = gridContainer.querySelectorAll('.grid-item');
+  gridItems.forEach(item => item.style.backgroundColor = 'transparent');
+}
+
